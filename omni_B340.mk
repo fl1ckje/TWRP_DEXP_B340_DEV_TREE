e679bdc58f23b36c5c19f5def29dedd1addefdca
fl1ckje <52666076+fl1ckje@users.noreply.github.com>
2019-07-27 10:06:32 +0800

    call inherit-product fixes

--- omni_B340.mk
+++ omni_B340.mk
@@ -1,13 +1,17 @@
-#$(call inherit-product, vendor/omni/config/common.mk)
+# Get the prebuilt list of APNs
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, device/DEXP/B340/B340.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+PRODUCT_COPY_FILES += \
+    device/DEXP/B340/recovery.fstab:recovery/root/recovery.fstab
 
 PRODUCT_DEVICE := B340
 PRODUCT_NAME := omni_B340
 PRODUCT_BRAND := DEXP
 PRODUCT_MODEL := B340
 PRODUCT_MANUFACTURER := DEXP
-
-ALLOW_MISSING_DEPENDENCIES := true
